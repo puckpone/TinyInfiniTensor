338c40c7ce682cfa6e144c9b7dbbcc8fd3209fae
puck <ponepuck@gmail.com>
2025-01-29 21:14:19 +0800

    complete allocator

--- src/core/allocator.cc
+++ src/core/allocator.cc
@@ -28,11 +28,33 @@ namespace infini
         IT_ASSERT(this->ptr == nullptr);
         // pad the size to the multiple of alignment
         size = this->getAlignedSize(size);
-
         // =================================== 作业 ===================================
         // TODO: 设计一个算法来分配内存，返回起始地址偏移量
         // =================================== 作业 ===================================
-
+        // Naive: Iterate through the free blocks to find a suitable block
+        for (auto it = free_blocks.begin(); it != free_blocks.end(); ++it) {
+            size_t block_start = it->first;
+            size_t block_size = it->second;
+            if (block_size >= size) {
+                // Allocate memory from this block
+                size_t allocated_addr = block_start;
+                // Update the free block map
+                if (block_size > size) {
+                    // Reduce the size of the free block
+                    free_blocks[block_start + size] = block_size - size;
+                }
+                free_blocks.erase(it);
+                // Update used memory
+                used += size;
+                if (used > peak) {
+                    peak = used;
+                }
+                return allocated_addr;
+            }
+        }
+        //Red-black tree
+    // If no suitable block is found, return 0 (or handle out-of-memory case)
+        std::cout << "No suitable block found" << std::endl;
         return 0;
     }
 
@@ -40,10 +62,34 @@ namespace infini
     {
         IT_ASSERT(this->ptr == nullptr);
         size = getAlignedSize(size);
-
+        
+        // Update used memory
+        used -= size;
         // =================================== 作业 ===================================
         // TODO: 设计一个算法来回收内存
         // =================================== 作业 ===================================
+        // Add the freed block to the free_blocks map
+        auto it = free_blocks.lower_bound(addr); // Find the first block with address >= addr
+
+        // Try to merge with the previous block
+        if (it != free_blocks.begin()) {
+            auto prev = std::prev(it); // Get the previous block
+            if (prev->first + prev->second == addr) {
+                addr = prev->first;
+                size += prev->second;
+                free_blocks.erase(prev);
+            }
+        }
+
+        // Try to merge with the next block
+        if (it != free_blocks.end() && addr + size == it->first) {
+            size += it->second;
+            free_blocks.erase(it);
+        }
+
+        // Insert the merged block into the free_blocks map
+        free_blocks[addr] = size;
+
     }
 
     void *Allocator::getPtr()
---
+++ worklog.md
@@ -0,0 +1,17 @@
+# 1.27
+I met a error when make:
+```
+make: *** No rule to make target `format', needed by`.PHONY'Stop.
+```
+The reason is the command 'make' will use the 'ninja' as default, the solution is as follows:
+```
+GNU "make" has been installed as "gmake""
+
+If you need to use it as "make", you can add a "gnubin" directory to your PATH from your bashrc like:
+
+PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
+```
+
+# 1.29
+
+completed allocator
